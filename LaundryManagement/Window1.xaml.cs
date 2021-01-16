780b7699d379546c64459605c96f65ad8dc7a0ca
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2021-01-16 19:48:29 +0800

    Change login implementation to CEFSharp.

--- LaundryManagement/Window1.xaml.cs
+++ LaundryManagement/Window1.xaml.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.ClearScript;
-using Microsoft.ClearScript.JavaScript;
-using Microsoft.ClearScript.V8;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,33 +22,6 @@ namespace LaundryManagement
 		public Window1()
 		{
 			InitializeComponent();
-		}
-
-		private static readonly HttpClient client = new HttpClient();
-
-		private async Task Login(string username, string password)
-		{
-			try
-			{
-				HttpResponseMessage response = await client.GetAsync("https://baidu.com");
-			}
-			catch (TimeoutException exception)
-			{
-				MessageBox.Show("超时" + exception.Message);
-			}
-			catch (Exception exception)
-			{
-				MessageBox.Show("用户名或密码错误" + exception.Message);
-			}
-		}
-
-		private async Task BtnLogin_ClickAsync(object sender, RoutedEventArgs e)
-		{
-			await Login(tbUsername.Text, tbPassword.Password);
-			// if (success)
-			Close();
-			// else
-			// ErrorMsg.Visibility = Visibility.Visible;
 		}
 	}
 }
