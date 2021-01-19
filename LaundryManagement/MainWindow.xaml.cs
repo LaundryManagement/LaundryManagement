e3758b6a799fd67a8af62e8d752a12d9beaf392c
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2021-01-19 13:35:36 +0800

    记录到类的包装以及修不完的重复读取问题😥

--- LaundryManagement/MainWindow.xaml.cs
+++ LaundryManagement/MainWindow.xaml.cs
@@ -1,15 +1,42 @@
 ﻿using CefSharp;
 using CefSharp.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Windows;
 
 namespace LaundryManagement
 {
+	internal class Record
+	{
+		public readonly string OrderNo;
+		public readonly int StoreNo;
+		public readonly string StoreName;
+		public readonly int DeviceNo;
+		public readonly string ServiceType;
+		public readonly double Paid;
+		public readonly double CardDiscount;
+
+		public Record(string SourceDataItem)
+		{
+			string[] split = SourceDataItem.Split('\t');
+			OrderNo = split[0];
+			StoreNo = Convert.ToInt32(split[1]);
+			StoreName = split[2];
+			DeviceNo = Convert.ToInt32(split[6]);
+			ServiceType = split[7];
+			Paid = split[9] == "-" ? 0 : Convert.ToDouble(split[9][1..]);
+			CardDiscount = split[10] == "-" ? 0 : Convert.ToDouble(split[10][1..]);
+		}
+	}
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private List<Record> records;
+
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -35,19 +62,26 @@ namespace LaundryManagement
 				return;
 			}
 			ChromiumWebBrowser browser = loginWindow.Browser;
-			List<string> SourceData = new List<string>();
+			List<string> SourceOrderNo = new List<string>(), SourceData = new List<string>();
 			while (true)
 			{
-				JavascriptResponse r = await browser.EvaluateScriptAsync("document.getElementsByTagName(\"tbody\")[0].innerText;");
-				string s = (string)r.Result;
-				string[] data = s.Split('\n');
-				foreach (string i in data)
+				JavascriptResponse r0 = await browser.EvaluateScriptAsync
+					("document.getElementsByTagName(\"tbody\")[0].innerText;");
+				JavascriptResponse r1 = await browser.EvaluateScriptAsync
+					("document.getElementsByTagName(\"tbody\")[1].innerText;");
+				string s0 = (string)r0.Result;
+				string s1 = (string)r1.Result;
+				string[] data = s0.Split('\n');
+				string[] orderNo = s1.Split('\n');
+				for (int i = 0; i < data.Length; i++)
 				{
-					SourceData.Add(i);
+					string s = orderNo[i] + '\t' + data[i];
+					SourceData.Add(s);
 				}
-				JavascriptResponse n = await browser.EvaluateScriptAsync
+				Thread.Sleep(200);
+				JavascriptResponse next = await browser.EvaluateScriptAsync
 					("document.getElementsByClassName(\" ant-pagination-next\")[0].attributes[\"aria-disabled\"].value");
-				if ((string)n.Result == "true")
+				if ((string)next.Result == "true")
 				{
 					MessageBox.Show($"共读取了{SourceData.Count}条数据");
 					break;
@@ -55,9 +89,25 @@ namespace LaundryManagement
 				else
 				{
 					browser.ExecuteScriptAsync("document.getElementsByClassName(\" ant-pagination-next\")[0].click()");
+					Thread.Sleep(200);
 				}
 			}
+			JavascriptResponse prev = await browser.EvaluateScriptAsync
+					("document.getElementsByClassName(\" ant-pagination-prev\")[0].attributes[\"aria-disabled\"].value");
+			while ((string)prev.Result == "false")
+			{
+				browser.ExecuteScriptAsync
+					("document.getElementsByClassName(\" ant-pagination-prev\")[0].click()");
+				Thread.Sleep(200);
+				prev = await browser.EvaluateScriptAsync
+					("document.getElementsByClassName(\" ant-pagination-prev\")[0].attributes[\"aria-disabled\"].value");
+			}
 			// TODO: 解析为类并放入数据库
+			records = new List<Record>();
+			foreach (string source in SourceData)
+			{
+				records.Add(new Record(source));
+			}
 		}
 	}
 }
